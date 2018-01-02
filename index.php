2f80c76673a8aa504ce6c9d93d4bb006335b6c12
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-01-02 17:48:14 -0200

    Não lista Usuários

--- index.php
+++ index.php
@@ -62,6 +62,71 @@
 
 	});
 
+	$app->get('/admin/users', function() {
+
+	    User::verifyLogin();
+
+		$users = User::listAll();
+
+		$page = new PageAdmin();
+
+		$page->setTpl("users", array(
+			"users"=>$users
+		));
+
+	});
+
+	$app->get('/admin/users/create', function() {
+
+		User::verifyLogin();
+
+		$page = new PageAdmin();
+
+		$page->setTpl("users-create");
+
+	});	
+
+	$app->get('/admin/users/:iduser/delete', function($iduser) {
+
+		User::verifyLogin();		
+
+	});
+
+	$app->get('/admin/users/:iduser', function($iduser) {
+
+		User::verifyLogin();
+
+		$page = new PageAdmin();
+
+		$page->setTpl("users-update");
+
+	});
+
+	$app->post('/admin/users/create', function() {
+
+		User::verifyLogin();		
+
+		$user = new User();
+
+		$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+		$_POST["nrphone"] = (int)$_POST["nrphone"];		
+
+		$user->setData($_POST);
+
+		$user->save();
+
+		header("Location: /admin/users");
+		exit;
+
+	});
+
+	$app->post('/admin/users/:iduser', function($iduser) {
+
+		User::verifyLogin();		
+
+	});
+
 	$app->run();
 
 ?>
