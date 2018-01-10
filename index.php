a83ad5b6750e772a9beea5308796112233d36307
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-01-09 22:15:26 -0200

    Recuperar Senha

--- index.php
+++ index.php
@@ -86,22 +86,19 @@
 
 	});	
 
-	$app->post('/admin/users/create', function() {
+	$app->post("/admin/users/create", function () {
 
-		User::verifyLogin();		
+	    User::verifyLogin();
+	    $user = new User();
+	    $_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
 
-		$user = new User();
-
-		$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
-
-		$_POST["nrphone"] = (int)$_POST["nrphone"];		
-
-		$user->setData($_POST);
-
-		$user->save();
-
-		header("Location: /admin/users");
-		exit;
+	    $_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+	        "cost"=>12
+	    ]);
+	    $user->setData($_POST);
+	    $user->save();
+	    header("Location: /admin/users");
+	    exit;
 
 	});
 
@@ -155,7 +152,77 @@
 
 	});
 
-	
+	$app->get('/admin/forgot', function() {
+
+		$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+		]);
+
+		$page->setTpl("forgot");
+
+	});
+
+	$app->post('/admin/forgot', function() {
+
+		$user = User::getForgot($_POST["email"]);
+
+		header("Location: /admin/forgot/sent");
+		exit;
+
+	});
+
+	$app->get('/admin/forgot/sent', function() {
+
+		$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+		]);
+
+		$page->setTpl("forgot-sent");
+
+	});
+
+	$app->get('/admin/forgot/reset', function() {
+
+		$user = User::validForgotDecrypt($_GET["code"]);
+
+		$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+		]);
+
+		$page->setTpl("forgot-reset", array(
+			"name"=>$user["desperson"],
+			"code"=>$_GET["code"]
+		));
+
+	});
+
+	$app->post('/admin/forgot/reset', function() {
+
+		$forgot = User::validForgotDecrypt($_POST["code"]);
+
+		User::setForgotUsed($forgot["idrecovery"]);
+
+		$user = new User();
+
+		$user->get((int)$forgot["iduser"]);
+
+		$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+			"cost"=>12
+		]);
+
+		$user->setPassword($password);
+
+		$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+		]);
+
+		$page->setTpl("forgot-reset-success");
+
+	});
 
 	$app->run();
 
