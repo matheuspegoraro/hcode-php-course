55f7ed99fe53ec78a4634c82d2f7304e1fddd94b
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-01-11 20:37:52 -0200

    Site - Categorias

--- index.php
+++ index.php
@@ -312,6 +312,21 @@
 
 	});
 
+	$app->get('/categories/:idcategory', function($idcategory) {
+
+		$category = new Category();
+
+		$category->get((int)$idcategory);
+
+		$page = new Page();
+
+		$page->setTpl("category", [
+			'category'=>$category->getValues(),
+			'products'=>[]
+		]);
+
+	});
+
 	$app->run();
 
 ?>
