ab758d2e9b4d0a8741554e52fea3c4c573b0734b
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-01-13 02:28:58 -0200

    Site Detalhes Produto

--- site.php
+++ site.php
@@ -47,4 +47,19 @@
 
 	});
 
+	$app->get('/products/:desurl', function($desurl) {
+
+		$product = new Product();
+
+		$product->getFromUrl($desurl);
+
+		$page = new Page();
+
+		$page->setTpl("product-detail", [
+			"product"=>$product->getValues(),
+			"categories"=>$product->getCategories()
+		]);
+
+	}); 
+
 ?>
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -150,6 +150,32 @@
 
 		}
 
+		public function getFromUrl($desurl)
+		{
+
+			$sql = new Sql();
+
+			$rows = $sql->select("SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1", [
+				":desurl"=>$desurl
+			]);
+
+			$this->setData($rows[0]);
+
+		}
+
+		public function getCategories()
+		{
+
+			$sql = new Sql();
+
+			return $sql->select("
+				SELECT * FROM tb_categories a INNER JOIN tb_productscategories b ON a.idcategory = b.idcategory WHERE b.idproduct = :idproduct
+			", [
+				":idproduct"=>$this->getidproduct()
+			]);
+
+		}
+
 	}
 
 ?>
