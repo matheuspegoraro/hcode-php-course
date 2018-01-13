b97c6023d796d00a27d0f9a4887c263400e6ef39
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-01-13 00:35:01 -0200

    Site - Produtos

--- site.php
+++ site.php
@@ -2,12 +2,17 @@
 
 	use \Hcode\Page;
 	use \Hcode\Model\Category;
+	use \Hcode\Model\Product;
 
 	$app->get('/', function() {
+
+		$products = Product::listAll();
 	    
 		$page = new Page();
 
-		$page->setTpl("index");
+		$page->setTpl("index", [
+			"products"=>Product::checkList($products)
+		]);
 
 	});
 
--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -17,6 +17,21 @@
 
 		}
 
+		public static function checkList($list)
+		{
+
+			foreach ($list as &$row) {
+				
+				$p = new Product();
+				$p->setData($row);
+				$row = $p->getValues();
+
+			}
+
+			return $list;
+
+		}
+
 		public function save()
 		{
 
