dc2a33a8b5895bbddf31abec03e0830e7e588ee0
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-01-13 02:06:34 -0200

    Site Paginação

--- site.php
+++ site.php
@@ -18,15 +18,31 @@
 
 	$app->get('/categories/:idcategory', function($idcategory) {
 
+		$page = (isset($_GET["page"])) ? (int)$_GET["page"] : 1;
+
 		$category = new Category();
 
 		$category->get((int)$idcategory);
 
+		$pagination = $category->getProductsPage($page);
+
 		$page = new Page();
 
+		$pages = [];
+
+		for ($i = 1; $i <= $pagination["pages"]; $i++) { 
+			
+			array_push($pages, [
+				"link"=>"/categories/".$category->getidcategory()."?page=".$i,
+				"page"=>$i
+			]);
+
+		}
+
 		$page->setTpl("category", [
-			'category'=>$category->getValues(),
-			'products'=>Product::checkList($category->getProducts())
+			"category"=>$category->getValues(),
+			"products"=>$pagination["data"],
+			"pages"=>$pages
 		]);
 
 	});
