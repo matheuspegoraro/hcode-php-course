6eba2fff142e9a2587383fc30fc03b9055c74f9b
Matheus Pegoraro <matheuspegoraro@icloud.com>
2018-01-15 00:18:58 -0200

    Site - Carrinho Calculo de frete

--- site.php
+++ site.php
@@ -72,7 +72,8 @@
 
 		$page->setTpl("cart", [
 			"cart"=>$cart->getValues(),
-			"products"=>$cart->getProducts()
+			"products"=>$cart->getProducts(),
+			"error"=>Cart::getMsgError()
 		]);
 
 	});
@@ -122,6 +123,17 @@
 		$cart = Cart::getFromSession();
 
 		$cart->removeProduct($product, true);
+
+		header("Location: /cart");
+		exit;
+
+	});
+
+	$app->post('/cart/freight', function() {
+
+		$cart = Cart::getFromSession();
+
+		$cart->setFreight($_POST["zipcode"]);
 
 		header("Location: /cart");
 		exit;
